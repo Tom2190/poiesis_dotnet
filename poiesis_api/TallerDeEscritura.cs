f4299c7de2606826196a4280fadc50034c034533
Tomas Fernandez <fernandez.abrevaya@gmail.com>
2020-10-10 16:58:22 -0300

    clases básicas de project

---
+++ poiesis_api/TallerDeEscritura.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace poiesis_api
+{
+    class TallerDeEscritura
+    {
+        public void filtrarTextos()
+        {
+
+        }
+        public void signIn()
+        {
+
+        }
+        public void mostrarTexto()
+        {
+
+        }
+
+        public void mostrarInfoTaller()
+        {
+
+        }
+    }
+}
---
+++ poiesis_api/Texto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace poiesis_api
+{
+    class Texto
+    {
+
+        private int idTexto { get; set;}
+        private Usuario autor { get; set; }
+        private DateTime fechaPublicacion { get; set; }
+        private String titulo { get; set; }
+        private String contenido { get; set; }
+    }
+}
