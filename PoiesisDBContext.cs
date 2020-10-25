004526eec58ce989b60862b5673a56bee38b0513
Priscila Bey <pri.bey@gmail.com>
2020-10-25 20:49:07 -0300

    cambio configs

--- PoiesisDBContext.cs
+++ PoiesisDBContext.cs
@@ -14,8 +14,7 @@ namespace poiesis_api
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Server=localhost;Database=PoiesisDB;User=sa;Password=hola1234;");
-                // optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=SchoolDB;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=SchoolDB;Trusted_Connection=True;");
             }
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
